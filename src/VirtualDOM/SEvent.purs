636536fab2af2e6070d59963ee56f43ed2eaf2d2
FrigoEU <simon.van.casteren@gmail.com>
2017-01-08 10:51:38 +0100

    Exports mkUIHandlers'

--- src/VirtualDOM/SEvent.purs
+++ src/VirtualDOM/SEvent.purs
@@ -1,4 +1,4 @@
-module VirtualDOM.SEvent (on, on', mkUIHandlers, SEvent(..), SEventS, click, change, submit, magic) where
+module VirtualDOM.SEvent (on, on', mkUIHandlers, SEvent(..), SEventS, click, change, submit, magic, mkUIHandlers') where
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (throw, EXCEPTION)
