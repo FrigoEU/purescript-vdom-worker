f098927d174a5e3bd19719c5b3301629e09e821f
FrigoEU <simon.van.casteren@gmail.com>
2017-02-03 09:49:01 +0100

    Adds lazyRefs

--- src/VirtualDOM/Lazy.js
+++ src/VirtualDOM/Lazy.js
@@ -1,3 +1,87 @@
 "use strict";
 
-// module VirtualDOM.Lazy
+/* Stolen from https://github.com/evancz/virtual-dom/blob/master/src/wrapper.js */
+function Thunk(fn, args, thunk) {
+  /* public (used by VirtualDom.js) */
+  this.vnode = null;
+  this.key = undefined;
+
+  /* private */
+  this.fn = fn;
+  this.args = args;
+  this.thunk = thunk;
+}
+
+Thunk.prototype.type = "Thunk";
+Thunk.prototype.render = renderThunk;
+
+function shouldUpdate(current, previous) {
+  if (current.fn !== previous.fn) {
+    return true;
+  }
+
+  // if it's the same function, we know the number of args must match
+  var cargs = current.args;
+  var pargs = previous.args;
+
+  for (var i = cargs.length; i--; ) {
+    if (cargs[i] !== pargs[i]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function renderThunk(previous) {
+  if (previous == null || shouldUpdate(this, previous)) {
+    return this.thunk();
+  } else {
+    return previous.vnode;
+  }
+}
+exports.lazyRef1Impl = function lazyRef1Impl(fn, a) {
+  function thunk() {
+    return fn(a);
+  }
+  return new Thunk(fn, [a], thunk);
+};
+
+exports.lazyRef2Impl = function lazyRef2Impl(fn, a, b) {
+  function thunk() {
+    return fn(a)(b);
+  }
+  return new Thunk(fn, [a,b], thunk);
+};
+
+exports.lazyRef3Impl = function lazyRef3Impl(fn, a, b, c) {
+  function thunk() {
+    return fn(a)(b)(c);
+  }
+  return new Thunk(fn, [a,b,c], thunk);
+};
+exports.lazyRef4Impl = function lazyRef4Impl(fn, a, b, c, d) {
+  function thunk() {
+    return fn(a)(b)(c)(d);
+  }
+  return new Thunk(fn, [a,b,c,d], thunk);
+};
+exports.lazyRef5Impl = function lazyRef5Impl(fn, a, b, c, d, e) {
+  function thunk() {
+    return fn(a)(b)(c)(d)(e);
+  }
+  return new Thunk(fn, [a,b,c,d,e], thunk);
+};
+
+exports.lazyRef6Impl = function lazyRef6Impl(fn, a, b, c, d, e, f) {
+  function thunk() {
+    return fn(a)(b)(c)(d)(e)(f);
+  }
+  return new Thunk(fn, [a,b,c,d,e,f], thunk);
+};
+exports.lazyRef7Impl = function lazyRef7Impl(fn, a, b, c, d, e, f, g) {
+  function thunk() {
+    return fn(a)(b)(c)(d)(e)(f)(g);
+  }
+  return new Thunk(fn, [a,b,c,d,e,f,g], thunk);
+};
--- src/VirtualDOM/Lazy.purs
+++ src/VirtualDOM/Lazy.purs
@@ -1,9 +1,26 @@
 module VirtualDOM.Lazy where
 
-{-- lazyRef1 --}
-{-- lazyRef2 --}
-{-- lazyRef3 --}
-{-- lazyRef4 --}
-{-- lazyRef5 --}
+import Data.Function.Uncurried (Fn2, Fn3, Fn4, Fn5, Fn6, Fn7, Fn8, runFn2, runFn3, runFn4, runFn5, runFn6, runFn7, runFn8)
+import VirtualDOM (VTree)
 
-{-- lazyObj --}
+foreign import lazyRef1Impl :: forall a. Fn2 (a -> VTree) a VTree
+lazyRef1 :: forall a. (a -> VTree) -> a -> VTree
+lazyRef1 = runFn2 lazyRef1Impl
+foreign import lazyRef2Impl :: forall a b. Fn3 (a -> b -> VTree) a b VTree
+lazyRef2 :: forall a b. (a -> b -> VTree) -> a -> b -> VTree
+lazyRef2 = runFn3 lazyRef2Impl
+foreign import lazyRef3Impl :: forall a b c. Fn4 (a -> b -> c -> VTree) a b c VTree
+lazyRef3 :: forall a b c. (a -> b -> c -> VTree) -> a -> b -> c -> VTree
+lazyRef3 = runFn4 lazyRef3Impl
+foreign import lazyRef4Impl :: forall a b c d. Fn5 (a -> b -> c -> d -> VTree) a b c d VTree
+lazyRef4 :: forall a b c d. (a -> b -> c -> d -> VTree) -> a -> b -> c -> d -> VTree
+lazyRef4 = runFn5 lazyRef4Impl
+foreign import lazyRef5Impl :: forall a b c d e. Fn6 (a -> b -> c -> d -> e -> VTree) a b c d e VTree
+lazyRef5 :: forall a b c d e. (a -> b -> c -> d -> e -> VTree) -> a -> b -> c -> d -> e -> VTree
+lazyRef5 = runFn6 lazyRef5Impl
+foreign import lazyRef6Impl :: forall a b c d e f. Fn7 (a -> b -> c -> d -> e -> f -> VTree) a b c d e f VTree
+lazyRef6 :: forall a b c d e f. (a -> b -> c -> d -> e -> f -> VTree) -> a -> b -> c -> d -> e -> f -> VTree
+lazyRef6 = runFn7 lazyRef6Impl
+foreign import lazyRef7Impl :: forall a b c d e f g. Fn8 (a -> b -> c -> d -> e -> f -> g -> VTree) a b c d e f g VTree
+lazyRef7 :: forall a b c d e f g. (a -> b -> c -> d -> e -> f -> g -> VTree) -> a -> b -> c -> d -> e -> f -> g -> VTree
+lazyRef7 = runFn8 lazyRef7Impl
